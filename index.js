25ded5e5e35f28fb6a64d4ccf9ff2a23bc4b02fb
Neil Empey <nempey@eventpower.com>
2022-10-31 14:46:17 -0600

    cc

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const github = require('@actions/github');
 
 const main = async () => {
     try {
-        const owner = core.getInput('owner', { required: true });
-        const repo = core.getInput('repo', { required: true });
+        const files = core.getInput('files', { required: true });
         const token = core.getInput('token', { required: true });
+
+        console.log('PIZZA')
+        console.log(files)
+        console.log('PIZZA')
 
         const octokit = new Octokit({
             auth: token,
         })
 
-        console.log(await octokit.request('GET /repos/{owner}/{repo}/commits', {
-            owner: owner,
-            repo: repo,
-        }))
     } catch (error) {
         core.setFailed(error.message);
     }
