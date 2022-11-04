48f6bb39eb5dd9fa4587f85e441141f5328dd08d
Neil Empey <nempey@eventpower.com>
2022-11-04 11:16:49 -0600

    test1

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
-const main = async () => {
-    try {
-        const files = core.getInput('files', { required: true });
-        const token = core.getInput('token', { required: true });
-
-        core.setOutput('big_change', files)
-
-        const octokit = new Octokit({
-            auth: token,
-        })
-
-    } catch (error) {
-        core.setFailed(error.message);
-    }
-
+async function run() {
+    const temp = core.getInput('temp');
+    console.log(`The temperature is ${temp} degrees.`);
 }
+
+run();
+
