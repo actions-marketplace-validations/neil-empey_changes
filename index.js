97955a28fbd43645c55e53cb23846f9eb697a423
Neil Empey <nempey@eventpower.com>
2022-11-04 12:57:17 -0600

    index

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const core = require('@actions/core');
 async function run() {
     const files = core.getInput('changed-files');
     const emails = core.getInput('emails');
+    const notify = core.setOutput('notify', true);
 
-    console.log(files);
-    console.log(emails);
+    if (files.includes('No Changes')) {
+        let changedFiles = files.split(',');
+        fileTypeArray = ['html', 'css'];
+
+        for (let i = 0; i < changedFiles.length; i++) {
+            fileTypeArray.map(type => {
+                if (type.includes(changedFiles[i])) {
+                    notify(emails);
+                }
+            });
+        }
+    }
 }
 
 run();
